3d1756071d7685de44848907a053f46f30b04078
indirectlylit <devon@indirectlylit.com>
2016-02-22 20:19:17 -0800

    upgrading starter project to Django 1.9.2

--- starter/urls.py
+++ starter/urls.py
@@ -1,9 +1,25 @@
-from django.conf.urls import patterns, include, url
+"""starter URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns(__package__ + '.views',
-    url(r'^$', 'landing_page', {}, 'landing_page'),
-    url(r'^admin/', include(admin.site.urls)),
-)
+from starter import views
+
+urlpatterns = [
+    url(r'^$', views.landing_page, {}, name='landing_page'),
+    url(r'^admin/', admin.site.urls),
+]
