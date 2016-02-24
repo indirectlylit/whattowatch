31a4bc2a927229a08126045212e1321936bac398
indirectlylit <devon@indirectlylit.com>
2016-02-23 18:39:16 -0800

    clean up templates, views, urls

--- starter/views.py
+++ starter/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def landing_page(request):
-    return render(request, "starter/landing_page.html")
+
+def main(request):
+    return render(request, "main.html")
